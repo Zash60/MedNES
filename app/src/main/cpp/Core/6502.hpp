2d57270ced491a65bbbd93cef4f54e76484ad3eb
Zash60 <xath06@gmail.com>
2025-12-03 15:44:02 -0300

    Update 6502.hpp

--- app/src/main/cpp/Core/6502.hpp
+++ app/src/main/cpp/Core/6502.hpp
@@ -10,7 +10,7 @@
 #include "Mapper/Mapper.hpp"
 #include "PPU.hpp"
 #include "RAM.hpp"
-#include "APU.hpp" // Integração de Áudio
+#include "APU.hpp"
 
 namespace MedNES {
 
@@ -29,7 +29,7 @@ class CPU6502 {
         READ,
         WRITE
     };
-    
+
     enum StatusFlags {
         NEGATIVE = 7,
         OVERFLO = 6,
@@ -42,7 +42,6 @@ class CPU6502 {
     };
 
    public:
-    // Construtor atualizado para receber APU
     CPU6502(Mapper *mapper, PPU *ppu, APU* apu, Controller *controller) 
         : mapper(mapper), ppu(ppu), apu(apu), controller(controller){};
         
@@ -71,12 +70,11 @@ class CPU6502 {
     RAM ram;
     Mapper *mapper;
     PPU *ppu;
-    APU *apu; // Ponteiro para APU
+    APU *apu;
     Controller *controller;
 
     std::stringstream execLog;
 
-    // Helpers Inline
     inline void setSRFlag(StatusFlags, bool);
     inline void setNegative(bool);
     inline void setOverflow(bool);
@@ -86,21 +84,20 @@ class CPU6502 {
     inline void setInterruptDisable(bool);
     inline void setZero(bool);
     inline void setCarry(bool);
-    
+
     inline void irq();
     inline void NMI();
-    
     inline void LOG_EXEC(u8 instr);
     inline void LOG_PC();
     inline void LOG_CPU_STATE();
     inline void PRINT_LOG();
-    
     inline void tick();
-    
+    inline void pushPC();
+
     void pushStack(u8);
     u8 popStack();
-    
-    // Addressing Modes
+
+    // --- Addressing Modes (Retornam endereço) ---
     u16 immediate();
     u16 zeroPage();
     u16 zeroPageX();
@@ -111,12 +108,20 @@ class CPU6502 {
     u16 indirectX();
     u16 indirectY(bool);
     u16 relative();
-    
-    // Instruções (Otimizadas, sem std::function)
-    void ADC(u8); 
+
+    // --- Instructions (Otimizadas: Recebem valor ou endereço direto) ---
+    void ADC(u8);
     void AND(u8);
-    u8 ASL_val(u8);
     
+    // As instruções RMW agora retornam o valor calculado ou modificam internamente
+    u8 ASL_val(u8);
+    u8 LSR_val(u8);
+    u8 ROL_val(u8);
+    u8 ROR_val(u8);
+    u8 INC_val(u8);
+    u8 DEC_val(u8);
+
+    // Branch Helpers
     void commonBranchLogic(bool, u16);
     void BCC(u16);
     void BCS(u16);
@@ -126,55 +131,58 @@ class CPU6502 {
     void BPL(u16);
     void BVC(u16);
     void BVS(u16);
-    
+
     void BIT(u16);
     void BRK();
     void CLC();
     void CLD();
     void CLI();
     void CLV();
+    
     void CMP(u8);
     void CPX(u8);
     void CPY(u8);
-    u8 DEC_val(u8);
+    
     void DEX();
     void DEY();
     void EOR(u8);
-    u8 INC_val(u8);
     void INX();
     void INY();
+    
     void JMP(u16);
-    void JMP_Indirect();
+    void JMP_Indirect(); // Separate logic
     void JSR(u16);
+    
     void LDA(u8);
     void LDX(u8);
     void LDY(u8);
-    u8 LSR_val(u8);
+    
     void NOP();
     void ORA(u8);
     void PHA();
     void PHP();
     void PLA();
     void PLP();
-    u8 ROL_val(u8);
-    u8 ROR_val(u8);
     void RTI();
     void RTS();
     void SBC(u8);
     void SEC();
     void SED();
     void SEI();
+    
+    // Store usa endereço
     void STA(u16);
     void STX(u16);
     void STY(u16);
+    
     void TAX();
     void TAY();
     void TSX();
     void TXA();
     void TXS();
     void TYA();
-    
-    // Unofficial
+
+    // Unofficial Helpers
     void LAX(u16);
     void SAX(u16);
     void DCP(u16);
@@ -183,9 +191,8 @@ class CPU6502 {
     void RLA(u16);
     void RRA(u16);
     void SRE(u16);
-    
+
     void tickIfToNewPage(u16, u16);
-    inline void pushPC();
 };
 
 };  //namespace MedNES
